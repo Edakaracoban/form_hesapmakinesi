696bb8a8e7f07c5c2441d40255c9bb3c470a2fee
EDANIN-DESKTOPU <edakaracoban1997@gmail.com>
2024-07-04 23:47:54 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.txt_2 = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(12, 30);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(49, 18);
+            this.lbl_1.TabIndex = 0;
+            this.lbl_1.Text = "Sayı 1";
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(12, 73);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(49, 18);
+            this.lbl_2.TabIndex = 1;
+            this.lbl_2.Text = "Sayı 2";
+            this.lbl_2.Click += new System.EventHandler(this.lbl_2_Click);
+            // 
+            // txt_1
+            // 
+            this.txt_1.Location = new System.Drawing.Point(72, 24);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.Size = new System.Drawing.Size(100, 24);
+            this.txt_1.TabIndex = 2;
+            // 
+            // txt_2
+            // 
+            this.txt_2.Location = new System.Drawing.Point(72, 67);
+            this.txt_2.Name = "txt_2";
+            this.txt_2.Size = new System.Drawing.Size(100, 24);
+            this.txt_2.TabIndex = 3;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(192, 20);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(75, 25);
+            this.btn_1.TabIndex = 4;
+            this.btn_1.Text = "Toplama";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(192, 51);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 30);
+            this.btn_2.TabIndex = 5;
+            this.btn_2.Text = "Çıkarma";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(192, 87);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 23);
+            this.btn_3.TabIndex = 6;
+            this.btn_3.Text = "Bölme";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(192, 116);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 25);
+            this.btn_4.TabIndex = 7;
+            this.btn_4.Text = "Çarpma";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(15, 171);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(252, 35);
+            this.btn_5.TabIndex = 8;
+            this.btn_5.Text = "Çıkış";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Sonuç";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_4
+            // 
+            this.lbl_4.AutoSize = true;
+            this.lbl_4.ForeColor = System.Drawing.Color.Crimson;
+            this.lbl_4.Location = new System.Drawing.Point(72, 114);
+            this.lbl_4.Name = "lbl_4";
+            this.lbl_4.Size = new System.Drawing.Size(0, 18);
+            this.lbl_4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(287, 227);
+            this.Controls.Add(this.lbl_4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.txt_2);
+            this.Controls.Add(this.txt_1);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.TextBox txt_2;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hoşgeldiniz");
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(txt_1.Text);
+            sayi2 = Convert.ToDouble(txt_2.Text);
+            sonuc=sayi1 + sayi2;
+            lbl_4.Text=sonuc.ToString();
+        }
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(txt_1.Text);
+            sayi2 = Convert.ToDouble(txt_2.Text);
+            sonuc = sayi1 - sayi2;
+            lbl_4.Text = sonuc.ToString();
+
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(txt_1.Text);
+            sayi2 = Convert.ToDouble(txt_2.Text);
+            if (sayi2!=0)
+            {
+                sonuc = sayi1 / sayi2;
+            }
+            else
+            {
+                MessageBox.Show("0'a bölüm işlemi tanımsızdır.");
+            }
+         
+            lbl_4.Text = sonuc.ToString();
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = Convert.ToDouble(txt_1.Text);
+            sayi2 = Convert.ToDouble(txt_2.Text);
+            sonuc = sayi1 *sayi2;
+            lbl_4.Text = sonuc.ToString();
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
